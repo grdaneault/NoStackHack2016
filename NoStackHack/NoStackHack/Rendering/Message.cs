2e8f4f11b81b649c106783a31d0cf6af87bf4b14
Greg Daneault <gregdaneault@gmail.com>
2016-11-06 01:37:35 -0500

    Basic font support

---
+++ NoStackHack/NoStackHack/Rendering/Message.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace NoStackHack.Rendering
+{
+    class Message
+    {
+        private Func<string> _updateMessage;
+        private Func<Vector2> _updateLocation;
+        public Vector2 Location => _updateLocation();
+        public string Text => _updateMessage();
+
+        public Message(Vector2 location, string message) : this(() => location, () => message) { }
+
+        public Message(Func<Vector2> location, string message) : this(location, () => message) { }
+
+        public Message(Vector2 location, Func<string> message) : this(() => location, message) { }
+
+        public Message(Func<Vector2> location, Func<string> message)
+        {
+            _updateMessage = message;
+            _updateLocation = location;
+        }
+    }
+}
